6ff1a196061432e24e0cd5c670d372824fb3834d
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-11-11 15:39:13 +0100

    renaming folder

---
+++ src/Amadeus.Server/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Amadeus.Server.Models;
+using Microsoft.AspNetCore.Routing;
+
+namespace Amadeus.Server.Controllers
+{
+	[ApiController]
+	[Route("home")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;https://docs.microsoft.com/en-us/aspnet/core/web-api/?view=aspnetcore-6.0#attribute-routing-requirement
+        }
+
+        [HttpGet("index")]
+        public string Index()
+        {
+            return "slt";
+        }
+
+        public string Privacy()
+        {
+            return "privacy";
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public string Error()
+        {
+	        return "error";
+	        //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
