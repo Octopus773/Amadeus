db5c77b3ec74bc0c57ac8c36f2e1b0fb482cdf12
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-11-12 17:20:45 +0100

    user service finished

--- src/Amadeus.Server/Models/User.cs
+++ src/Amadeus.Server/Models/User.cs
@@ -1,13 +1,53 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Amadeus.Server.Models
 {
+	/// <summary>
+	/// The user of all the dashboard.
+	/// </summary>
 	public class User
 	{
-		public UInt64 uid {get; set; }
-		public String username { get; set; }
-		public String displayName { get; set; }
-		public String email { get; set; }
-		public String password { get; set; }
+		/// <summary>
+		/// The primary key for the db.
+		/// </summary>
+		public ulong Id { get; set; }
+
+		/// <summary>
+		/// User's username.
+		/// </summary>
+		public string Username { get; set; }
+
+		/// <summary>
+		/// The name to use as a display.
+		/// </summary>
+		[Required]
+		public string DisplayName { get; set; }
+
+		/// <summary>
+		/// The user's email
+		/// </summary>
+		[Required]
+		public string Email { get; set; }
+
+		/// <summary>
+		/// The user's password.
+		/// </summary>
+		/// <note type="caution">
+		/// It should encoded.
+		/// </note>
+		[Required]
+		public string Password { get; set; }
+
+
+		/// <summary>
+		/// The creation time of the user.
+		/// </summary>
+		public DateTime CreatedAt { get; set; }
+
+		/// <summary>
+		/// True if the user is verified
+		/// </summary>
+		public bool Verified { get; set; }
 	}
 }
--- src/Amadeus.Server/Services/UserService.cs
+++ src/Amadeus.Server/Services/UserService.cs
@@ -1,23 +1,82 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Sockets;
+using System.Threading.Tasks;
+using Amadeus.Server.Data;
 using Amadeus.Server.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileSystemGlobbing.Internal.PathSegments;
 
 namespace Amadeus.Server.Services
 {
-	public static class UserService
+	public class UserService
 	{
-		private static List<User> _users { get; }
-		private static UInt64 _currentUid;
+		/// <summary>
+		/// The User ORM.
+		/// </summary>
+		private readonly UserDB _context;
 
-		public static List<User> getAll() => _users;
 
-		#nullable enable
-		public static User? get(UInt64 uid) => _users.FirstOrDefault(el => el.uid == uid);
-		#nullable disable
+		/// <summary>
+		/// Get all the users in db.
+		/// </summary>
+		/// <returns>All the users in the db.</returns>
+		public Task<List<User>> GetAll()
+		{
+			return _context.Users
+				.AsNoTracking()
+				.Select(o => new User
+				{
+					Username = o.Username,
+					Email = o.Email
+				})
+				.ToListAsync();
+		}
 
-		public static void add(User user) => _users.Add(user);
+		/// <summary>
+		/// Get a user by it's id.
+		/// </summary>
+		/// <param name="id">The id of the user to get.</param>
+		/// <returns>The user corresponding at the id.</returns>
+		public Task<User> GetUserById(ulong id)
+		{
+			return _context.Users.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+		}
+
+		/// <summary>
+		/// Create saves the user in the db.
+		/// </summary>
+		/// <param name="user">The user to save in the db.</param>
+		/// <returns>The user saved in db.</returns>
+		public async Task<User> Create(User user)
+		{
+			Debug.Assert(user != null, nameof(user) + " != null");
+			user.CreatedAt = DateTime.UtcNow;
+			_context.Add(user);
+			await _context.SaveChangesAsync();
+			return user;
+		}
+
+		/// <summary>
+		/// Delete a user by id.
+		/// </summary>
+		/// <param name="id">id of the user to delete.</param>
+		/// <returns>True if the user was deleted false otherwise</returns>
+		public async Task<bool> Delete(ulong id)
+		{
+			bool isDeleted = false;
+			User user = await GetUserById(id);
+
+			if (user != null)
+			{
+				_context.Remove(user);
+				await _context.SaveChangesAsync();
+				isDeleted = true;
+			}
+
+			return isDeleted;
+		}
 	}
 }
