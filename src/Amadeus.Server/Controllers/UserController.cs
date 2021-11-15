dce60ba08dec416d2d9a6a4643398345177d65b6
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-11-15 10:46:27 +0100

    fixing norm issues un UserController.cs

--- src/Amadeus.Server/Controllers/UserController.cs
+++ src/Amadeus.Server/Controllers/UserController.cs
@@ -5,36 +5,64 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Amadeus.Server.Controllers
 {
+	/// <summary>
+	/// The controller for user related interactions.
+	/// </summary>
 	[ApiController]
 	[Route("user")]
 	public class UserController : ControllerBase
 	{
+		/// <summary>
+		/// Get all the registered users.
+		/// </summary>
+		/// <returns>The list of all registered users.</returns>
 		[HttpGet]
-		public ActionResult<List<User>> getUsers()
+		public ActionResult<List<User>> GetUsers()
 		{
 			return new List<User>();
 		}
 
-		[HttpGet("/{uid:int}")]
-		public IActionResult getUser(UInt64 uid)
+		/// <summary>
+		/// Get a specific user.
+		/// </summary>
+		/// <param name="uid">Uid of the specific user.</param>
+		/// <returns>All infos of the specific user.</returns>
+		[HttpGet("/{uid:long}")]
+		public IActionResult GetUser(ulong uid)
 		{
 			return NotFound();
 		}
 
+		/// <summary>
+		/// Create a user.
+		/// </summary>
+		/// <param name="user">The user to create.</param>
+		/// <returns>The infos of the newly created user.</returns>
 		[HttpPost]
-		public IActionResult createUser(User user)
+		public IActionResult CreateUser(User user)
 		{
-			return CreatedAtAction(nameof(createUser), new { uid = 42 }, user);
+			return CreatedAtAction(nameof(CreateUser), new { uid = 42 }, user);
 		}
 
-		[HttpPut("/{uid:int}")]
-		public IActionResult modifyUser(UInt64 uid, User user)
+		/// <summary>
+		/// Modify a specific user.
+		/// </summary>
+		/// <param name="uid">The id of the user to modify.</param>
+		/// <param name="user">The new infos to update.</param>
+		/// <returns>The specific user with updated infos.</returns>
+		[HttpPut("/{uid:long}")]
+		public IActionResult ModifyUser(ulong uid, User user)
 		{
 			return NotFound();
 		}
 
-		[HttpDelete("/{uid:int}")]
-		public IActionResult deleteUser(UInt64 uid)
+		/// <summary>
+		/// Delete a specific user.
+		/// </summary>
+		/// <param name="uid">The id of the specific user.</param>
+		/// <returns>The infos of the deleted user.</returns>
+		[HttpDelete("/{uid:long}")]
+		public IActionResult DeleteUser(ulong uid)
 		{
 			return NoContent();
 		}
